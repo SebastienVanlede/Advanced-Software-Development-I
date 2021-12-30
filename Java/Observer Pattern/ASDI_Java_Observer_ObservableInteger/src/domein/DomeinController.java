f16e5741f36252dd533165467e554fe44a7d4bf7
Sebastien Vanlede <56797089+SebastienVanlede@users.noreply.github.com>
2021-12-30 20:41:30 +0100

    Adding observer pattern

---
+++ Java/Observer Pattern/ASDI_Java_Observer_ObservableInteger/src/domein/DomeinController.java
@@ -0,0 +1,24 @@
+package domein;
+
+import gui.ObservableIntegerFrameController;
+
+public class DomeinController {
+
+    private ObservableInteger observableInteger = new ObservableInteger();
+
+    public void up() {
+        observableInteger.add(1);
+    }
+
+    public void down() {
+        observableInteger.subtract(1);
+    }
+
+    public int getDoubleValue() {
+        return observableInteger.getDoubleValue();
+    }
+
+    public void addObserver(Observer observer){
+        observableInteger.addObserver(observer);
+    }
+}
